2c13d6c3c0501fc6546cd934962a1fdf232fa219
cognite-ornellas <rodrigo.ornellas@cognite.com>
2022-08-09 12:15:05 -0300

    chore: moving to right version

--- src/core/authentication/wrapper/service/CogniteProjectService.ts
+++ src/core/authentication/wrapper/service/CogniteProjectService.ts
@@ -1,35 +1,53 @@
 import { CogAuthContextProps } from "../CogAuthContextProps";
 import { CogniteProject } from "../CogniteProject";
 
+const { COGNITE_BASE_URL } = process.env;
+const { REACT_APP_COGNITE_BASE_URL } = process.env;
+
+const DEFAULT_TOKEN_INSPECT_ENDPOINT = '/api/v1/token/inspect';
+
+interface CogniteProjectOptions {
+  cogniteCluster?: string, 
+  cogniteBaseURL?: string
+}
 export class CogniteProjectService {
+    private tokenInspectURL: string;
+
+    constructor(options?: CogniteProjectOptions) {
+      if (options?.cogniteCluster) {
+        this.tokenInspectURL = `https://${options?.cogniteCluster}.cognitedata.com${DEFAULT_TOKEN_INSPECT_ENDPOINT}`
+      } else if (options?.cogniteBaseURL) {
+        this.tokenInspectURL = new URL(DEFAULT_TOKEN_INSPECT_ENDPOINT, options?.cogniteBaseURL).toString();
+      } else {
+        const baseUrl = REACT_APP_COGNITE_BASE_URL || COGNITE_BASE_URL;
+        if (!baseUrl) {
+          throw new Error(
+            "No cognite base URL was defined for token inspect."+
+            "Please use another constructor or provide an environment variable named as REACT_APP_COGNITE_BASE_URL or COGNITE_BASE_URL"
+          );
+        }
+
+        this.tokenInspectURL = new URL(DEFAULT_TOKEN_INSPECT_ENDPOINT, baseUrl).toString();
+      }
+    }
+
     public async loadFromAccessToken(accessToken: string | undefined): Promise<CogniteProject[]> {
         if(!accessToken)
           return [];
       
         const Authorization = `Bearer ${accessToken}`;
-        const response = await fetch('https://greenfield.cognitedata.com/api/v1/token/inspect', {
+        const response = await fetch(this.tokenInspectURL, {
           method: 'GET',  
           headers: {
             Authorization
           }
         })
-        const unknown = await response.json();
-        return await unknown.projects;
+        const tokenInspectResponse = await response.json();
+        return tokenInspectResponse.projects;
     }
     public async loadFromAuthContext(authContext: CogAuthContextProps): Promise<CogniteProject[]> {
       const accessToken: string | undefined = authContext?.user?.access_token;
 
-      if(!accessToken)
-          return [];
-      
-        const Authorization = `Bearer ${accessToken}`;
-        const response = await fetch('https://greenfield.cognitedata.com/api/v1/token/inspect', {
-          method: 'GET',  
-          headers: {
-            Authorization
-          }
-        })
-        const unknown = await response.json();
-        return await unknown.projects;
+      return await this.loadFromAccessToken(accessToken);
     }
 }
