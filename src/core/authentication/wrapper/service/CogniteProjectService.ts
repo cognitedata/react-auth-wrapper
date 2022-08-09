8c5496cd43a4d6d96bbc160a7af7f3020b3fd799
Rodrigo Ornellas <93260466+cognite-ornellas@users.noreply.github.com>
2022-08-09 11:34:55 -0300

    feat: improve project service (#12)
    Co-authored-by: cognite-ornellas <rodrigo.ornellas@cognite.com>

--- src/core/authentication/wrapper/service/CogniteProjectService.ts
+++ src/core/authentication/wrapper/service/CogniteProjectService.ts
@@ -1,11 +1,34 @@
 import { CogAuthContextProps } from "../CogAuthContextProps";
 import { CogniteProject } from "../CogniteProject";
 
+const { COGNITE_BASE_URL } = process.env;
+const { REACT_APP_COGNITE_BASE_URL } = process.env;
+
+const DEFAULT_TOKEN_INSPECT_ENDPOINT = '/api/v1/token/inspect';
+
+interface CogniteProjectOptions {
+  cogniteCluster?: string, 
+  cogniteBaseURL?: string
+}
 export class CogniteProjectService {
-    tokenInspectURL: string;
-    
-    constructor(protected cogniteCluster: string) {
-      this.tokenInspectURL = `https://${cogniteCluster}.cognitedata.com/api/v1/token/inspect`
+    private tokenInspectURL: string;
+
+    constructor(options?: CogniteProjectOptions) {
+      if (options?.cogniteCluster) {
+        this.tokenInspectURL = `https://${options?.cogniteCluster}.cognitedata.com${DEFAULT_TOKEN_INSPECT_ENDPOINT}`
+      } else if (options?.cogniteBaseURL) {
+        this.tokenInspectURL = new URL(DEFAULT_TOKEN_INSPECT_ENDPOINT, options?.cogniteBaseURL).toString();
+      } else {
+        const baseUrl = REACT_APP_COGNITE_BASE_URL || COGNITE_BASE_URL;
+        if (!baseUrl) {
+          throw new Error(
+            "No cognite base URL was defined for token inspect."+
+            "Please use another constructor or provide an environment variable named as REACT_APP_COGNITE_BASE_URL or COGNITE_BASE_URL"
+          );
+        }
+
+        this.tokenInspectURL = new URL(DEFAULT_TOKEN_INSPECT_ENDPOINT, baseUrl).toString();
+      }
     }
 
     public async loadFromAccessToken(accessToken: string | undefined): Promise<CogniteProject[]> {
@@ -19,8 +42,8 @@ export class CogniteProjectService {
             Authorization
           }
         })
-        const unknown = await response.json();
-        return await unknown.projects;
+        const tokenInspectResponse = await response.json();
+        return tokenInspectResponse.projects;
     }
     public async loadFromAuthContext(authContext: CogAuthContextProps): Promise<CogniteProject[]> {
       const accessToken: string | undefined = authContext?.user?.access_token;
