c6a24a1a8b421d98723d8c21db10809c4ae5a1bb
Rodrigo Ornellas <93260466+cognite-ornellas@users.noreply.github.com>
2022-08-09 10:10:32 -0300

    fix!: fixing static token inspect url (#11)
    Co-authored-by: cognite-ornellas <rodrigo.ornellas@cognite.com>

--- src/core/authentication/wrapper/service/CogniteProjectService.ts
+++ src/core/authentication/wrapper/service/CogniteProjectService.ts
@@ -2,12 +2,18 @@ import { CogAuthContextProps } from "../CogAuthContextProps";
 import { CogniteProject } from "../CogniteProject";
 
 export class CogniteProjectService {
+    tokenInspectURL: string;
+    
+    constructor(protected cogniteCluster: string) {
+      this.tokenInspectURL = `https://${cogniteCluster}.cognitedata.com/api/v1/token/inspect`
+    }
+
     public async loadFromAccessToken(accessToken: string | undefined): Promise<CogniteProject[]> {
         if(!accessToken)
           return [];
       
         const Authorization = `Bearer ${accessToken}`;
-        const response = await fetch('https://greenfield.cognitedata.com/api/v1/token/inspect', {
+        const response = await fetch(this.tokenInspectURL, {
           method: 'GET',  
           headers: {
             Authorization
@@ -19,17 +25,6 @@ export class CogniteProjectService {
     public async loadFromAuthContext(authContext: CogAuthContextProps): Promise<CogniteProject[]> {
       const accessToken: string | undefined = authContext?.user?.access_token;
 
-      if(!accessToken)
-          return [];
-      
-        const Authorization = `Bearer ${accessToken}`;
-        const response = await fetch('https://greenfield.cognitedata.com/api/v1/token/inspect', {
-          method: 'GET',  
-          headers: {
-            Authorization
-          }
-        })
-        const unknown = await response.json();
-        return await unknown.projects;
+      return await this.loadFromAccessToken(accessToken);
     }
 }
